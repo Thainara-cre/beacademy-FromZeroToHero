f1651f34e18e4e4197cf30221809efd466741531
Thainara da Silva <thainaracaracol@gmail.com>
2022-11-12 17:29:31 -0300

    Função parte 4

---
+++ JavaScript1/Aula-8/scripts.js
@@ -0,0 +1,9 @@
+// Função anônima
+
+(function() {
+    console.log(`PayLivre S2`)
+})()//auto execução
+
+setTimeout(function(){
+    console.log(`Vou levar 3 segundos para ser executado!`)
+},3000) // atrasa o tempo em milisegundos
