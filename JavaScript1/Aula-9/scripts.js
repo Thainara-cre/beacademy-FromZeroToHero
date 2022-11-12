d538c56184e01cba4076f375bc532841894a4d0b
Thainara da Silva <thainaracaracol@gmail.com>
2022-11-12 17:45:49 -0300

    Função parte 4

---
+++ JavaScript1/Aula-9/scripts.js
@@ -0,0 +1,47 @@
+// Parte 1
+
+//let assunto
+
+//function criarAssunto(assunto) {
+//    return assunto
+//}
+
+//console.log(criarAssunto(assunto))
+
+// Parte 2
+
+//let assunto = "Estudar"
+
+//function criarAssunto(assunto) {
+//    return assunto
+//}
+
+//console.log(assunto)
+//console.log(criarAssunto(assunto))
+
+// Parte 3
+/*
+let assunto = "Estudar"
+
+function criarAssunto(assunto) {
+    assunto = `Escutar musica`
+    return assunto
+}
+
+console.log(assunto)
+console.log(criarAssunto(assunto))
+console.log(assunto)
+*/
+
+// Parte 4
+
+let assunto = "Estudar"
+
+function criarAssunto() {
+    assunto = `Escutar musica`
+    return assunto
+}
+
+console.log(assunto)
+console.log(criarAssunto())
+console.log(assunto)
