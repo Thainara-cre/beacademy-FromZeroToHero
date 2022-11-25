98abd30299740e45ac2164981d8556fa9dd312b6
Thainara da Silva <thainaracaracol@gmail.com>
2022-11-25 10:15:25 -0300

    Variáveis

---
+++ JavaScriptIntermediário/aula-1/script.js
@@ -0,0 +1,30 @@
+/* SCOPE */
+
+{
+    let x = 2;
+    console.log(x)
+}
+
+{
+    let y = 2;
+}
+
+console.log(y)
+
+/* Reassign a value */
+
+let w = 10
+w = 5
+
+console.log(w)
+
+let firtName = "John";
+let lastName = "Doe";
+
+let text = `Welcome ${firtName}, ${lastName}`;
+console.log(text)
+
+let price = 10;
+let fees = 0.25;
+let total = `O total do produto é R${price + fees}`;
+console.log(total)
---
+++ JavaScriptIntermediário/aula-1/variaveis.md
@@ -0,0 +1,18 @@
+    Variáveis
+
+Variáveis são contêineres na memória para armazenar valores
+Declarando uma váriavel
+var x;
+
+Atribuindo valor 
+var x = 5;
+var y = 6;
+var z = x + y;
+
+var - 1995 a 2015
+
+let e const - 2015 ES6 
+Sempre usar const (ECMAScript 6)
+const x  = 5;
+const y = 6;
+let z = x + y;
